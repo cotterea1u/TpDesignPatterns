ff27ffe359692e4d55301df5b7fd0c4d4879915a
Maialen Cottereau <maialen.cottereau@telecomnancy.eu>
2018-12-03 11:20:09 +0100

    Configuration du projet

---
+++ src/visiteur/Media.java
@@ -0,0 +1,12 @@
+package visiteur;
+
+public abstract class Media {
+    protected int annee ;
+    protected String nom ;
+
+    protected Media(int a, String nom) {
+        this.annee = a;
+        this.nom = nom;
+    }
+
+}
